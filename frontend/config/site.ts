0c62439502c2c2425d1b60e4aecb4bab090e94cc
Will <5217539+WRansohoff@users.noreply.github.com>
2023-12-27 18:35:13 -0500

    Site header typo

--- frontend/config/site.ts
+++ frontend/config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: "TwootBookGen",
+	name: "TwootBookGram",
 	description: "Demo of how to run an AI service cheaply, with a small language model that can run within serverless functions.",
 	navItems: [
 	],
